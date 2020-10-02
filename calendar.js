e063aae57e8cc65d6b0039e3991248c1aee843fd
JosMuse <no-reply>
2020-10-02 17:46:17 -0600

    trying to get colors to pop up

--- calendar.js
+++ calendar.js
@@ -8,8 +8,9 @@ $(document).ready(function(){
     
         // Global variable section
 
-        var dateMoment = moment().format('dd, MMMM do, YYYY,h:mm a')
-        var hours = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
+        var date = dayjs().format('dddd, MMMM DD YYYY hh:m A')
+        console.log(date)
+        var hours = ["8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
         "5 p.m."]
         var calendar = document.querySelector(".container")
 
@@ -20,7 +21,7 @@ $(document).ready(function(){
 
         //this function calls todays date at the top of the page!
         function today(){
-            var today  = $("#currentDay").text(dateMoment)
+            var today  = $("#currentDay").text(date)
             timeblocks()
             return today
 
@@ -39,17 +40,42 @@ $(document).ready(function(){
 
         function timeblocks(){
         for (var i=0 ; i < hours.length; i++){
-        var row = $("<div>").addClass( "row")
-        var timeColumns =  $("<text-area>").text(hours[i]).addClass( "hour")
-        var inputColumn = $("<input>").attr("placeholder", "enter note here").addClass("todo-Input time-block")
-        savebtn = $("<button>").addClass("btn btn-primary saveBtn").text("save")
+        var row = $("<div>").addClass( "row col-12")
+        var timeColumns = $("<div>").text(hours[i]).addClass( "hour col-1").attr("id","hourChange")
+        var inputColumn = $("<input>").attr("placeholder", "enter note here").addClass("description time-block col-10")
+        var savebtn = $("<button>").addClass("btn btn-primary saveBtn col-1").text("save")
         $(row).append(timeColumns).append(inputColumn).append(savebtn)
         $("#timeblock").append(row)
 
         }
+
+        function currentHour(){
+            var currentTime = dayjs().format('hh:mm')
+             var currentTime2 = dayjs().isAfter(currentTime, 'hour')
+            console.log(currentTime)
+            
+            
+
+            $(".time-block").each(function(){
+
+                var divTime = ($(this).val())
+                console.log(divTime)
+
+                if (divTime < currentTime2){
+                    $(this).addClass("past")
+                } if (divTime == currentTime2){
+                    $(this).removeClass("past").addClass("present")
+                } if (divTime > currentTime2){
+                    $(this).removeClass("present past").addClass("future")
+                }
+
+
+            })
+
+            
+        }
+        currentHour()
     }
-
-
 
     })
 
@@ -60,12 +86,6 @@ $(document).ready(function(){
 
 
 
-//```
-//WHEN I scroll down
-//THEN I am presented with timeblocks for standard business hours
-//todo the div class container will house these timeblocks, they should go from 8 to 5 
 
 
 
-
-//
