684c0ecdaf19776711266d634cf0b18360b42699
JosMuse <no-reply>
2020-10-03 17:34:12 -0600

    finished all the work for the scheduler just need a read me

--- calendar.js
+++ calendar.js
@@ -7,79 +7,136 @@ $(document).ready(function(){
 
     
         // Global variable section
-
-        var date = dayjs().format('dddd, MMMM DD YYYY hh:m A')
-        console.log(date)
-        var hours = ["8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
+        var displayHours = ["8 a.m.", "9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
         "5 p.m."]
-        var calendar = document.querySelector(".container")
+        var hour = [8,9,10,11,12,13,14,15,16,17]
+        
+        //date today
+        var date = dayjs().format('dddd, MMMM DD YYYY hh:mm A')
+        
+        $("#currentDay").text(date)
 
-        var row 
-        var timeColumns
 
 
 
-        //this function calls todays date at the top of the page!
-        function today(){
-            var today  = $("#currentDay").text(date)
-            timeblocks()
-            return today
-
+        //object for saving to local storage
+        schedule = {
+            time8: "",
+            time9: "",
+            time10: "",
+            time11: "",
+            time12: "",
+            time13: "",
+            time14: "",
+            time15: "",
+            time16: "",
+            time17: "",
         }
 
-        today()
-
-
-       // WHEN I scroll down
-        //THEN I am presented with timeblocks for standard business hours
-        //todo the div class container will house these timeblocks, they should go from 8 to 5 
+        var scheduleStor = localStorage.getItem("schedule")
+        console.log(scheduleStor)
+        if (scheduleStor !==null){
+            schedule = JSON.parse(scheduleStor)
+            console.log(schedule)
+        }
 
         
-
-        //for loop to create timeblocks
-
+        
+        //This function will dynamically generate all of the timeblock section of the page.
         function timeblocks(){
-        for (var i=0 ; i < hours.length; i++){
-        var row = $("<div>").addClass( "row col-12")
-        var timeColumns = $("<div>").text(hours[i]).addClass( "hour col-1").attr("id","hourChange")
-        var inputColumn = $("<input>").attr("placeholder", "enter note here").addClass("description time-block col-10")
-        var savebtn = $("<button>").addClass("btn btn-primary saveBtn col-1").text("save")
-        $(row).append(timeColumns).append(inputColumn).append(savebtn)
-        $("#timeblock").append(row)
+            //create hours with loop
+            for (i=0; i< hour.length; i++){
+                var hourName = "hour" + hour[i]
+               
+                //timeblock element
+                var timeblockDiv = $("<div>")
+                timeblockDiv.addClass("row time-block").attr('id',"hour"+hour[i]).attr("value", hour[i])
 
-        }
+                //append to page
+                $(".container").append(timeblockDiv)
+                
 
-        function currentHour(){
-            var currentTime = dayjs().format('hh:mm')
-             var currentTime2 = dayjs().isAfter(currentTime, 'hour')
-            console.log(currentTime)
-            
-            
+                //time element
+                var timeCol = $("<div>")
+                timeCol.addClass("hour col-1").text(displayHours[i])
 
-            $(".time-block").each(function(){
+                //append to page
+                timeblockDiv.append(timeCol)
 
-                var divTime = ($(this).val())
-                console.log(divTime)
+                //description element
+                var descriptionCol = $("<textarea>")
+                descriptionCol.addClass("description col-10")
 
-                if (divTime < currentTime2){
-                    $(this).addClass("past")
-                } if (divTime == currentTime2){
-                    $(this).removeClass("past").addClass("present")
-                } if (divTime > currentTime2){
-                    $(this).removeClass("present past").addClass("future")
+                //append to page
+                timeblockDiv.append(descriptionCol)
+
+
+                //save element
+                var saveBtn = $("<button>")
+                saveBtn.addClass("saveBtn col-1").text("save")
+
+                //append to page
+                timeblockDiv.append(saveBtn)
+
+
+                // local storage to come up when the page loads
+                if(schedule[hourName]){
+                    descriptionCol.text(schedule[hourName])
                 }
-
-
-            })
-
-            
+                
+              
+            }
+            currentHour()
         }
-        currentHour()
+
+        //Update colors based on current time
+    function currentHour(){
+        var currentTime = moment().hours()
+        console.log(currentTime)
+        $(".time-block").each(function(){
+
+
+        //hours in the time block
+          blockTime = $(this).attr("value");
+         
+            //if statements will change the colors of the time blocks
+          if (blockTime < currentTime){
+            $(this).addClass("past")
+          } if (blockTime == currentTime){
+            $(this).removeClass("past")
+            $(this).addClass("present")
+          } if (blockTime > currentTime){
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+          }
+    
+        })
+    }
+    
+
+        timeblocks()
+
+
+
+    function saveItems (){
+        localStorage.setItem("schedule", JSON.stringify(schedule))
     }
 
+     //save button actually works maybe??
+     $(".saveBtn").on("click",function(){
+        value =$(this).siblings(".description").val()
+        key = $(this).parent().attr("id")
+
+        schedule[key] = value
+        saveItems()
     })
 
 
+        
+
+})   
+    
 
 
 
